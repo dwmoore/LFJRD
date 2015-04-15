98e2d7979a2d6cce1fcbb1531cdaf35f49c913e5
DAVID MOORE <khundawg@gmail.com>
2015-04-15 18:40:36 -0400

    Added jQuery_ujs to fix Devise issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,9 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
-
+//--- jQuery for Devise
+//= require jquery_ujs
+//
+//
 //--- Angle
 //= require_tree ./angle/
