eaa7e26b6b7a5dc9d726ee62f7264794d3f3a881
DAVID MOORE <khundawg@gmail.com>
2015-04-15 18:44:26 -0400

    Reset root route to visitors#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-#  root to: 'visitors#index'
+  root to: 'visitors#index'
   devise_for :users
 
   resources :profiles
 
   # defaults to dashboard
-  root :to => redirect('/singleview')
-  
+  #root :to => redirect('/singleview')
+
   # view routes
   get '/singleview' => 'singleview#index'
-  
+
   # api routes
   get '/api/i18n/:locale' => 'api#i18n'
 
